0d753cdb9d46bf95692f7439c7daa08bf54f7c17
Marco Galvan <elmacro11@gmail.com>
2023-11-28 18:03:00 -0300

    create Hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import Hero from "@/components/Hero/Hero";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.content_container}>
-        <h1>Inicio</h1>
+        <Hero />
       </div>
     </main>
   );
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,20 @@
+import styles from "./Hero.module.css";
+
+const Hero = () => {
+  return (
+    <section className={styles.main_container}>
+      <div>
+        <h1>Desarrollador Web Full Stack</h1>
+        <p>
+          Desarrollador web Full Stack apasionado por crear experiencias
+          digitales excepcionales con habilidades en diseño, implementación y
+          enfoque en la creatividad y eficiencia. Capaz de entender las
+          necesidades del cliente y ofrecer soluciones innovadoras con
+          usabilidad y experiencias positivas.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
